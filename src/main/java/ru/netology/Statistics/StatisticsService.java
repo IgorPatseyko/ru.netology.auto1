9db542b1cb6cc19633d68f8b1a2d059d3c49c5a4
igor test <igor.patseyko@mail.ru>
2023-08-30 15:12:40 +0200

    Homework 7.1

--- src/main/java/ru/netology/Statistics/StatisticsService.java
+++ src/main/java/ru/netology/Statistics/StatisticsService.java
@@ -1,4 +1,17 @@
 package ru.netology.Statistics;
 
 public class StatisticsService {
+    public long findMax(long[] incomes) {
+        if (incomes.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
 }
