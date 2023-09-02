c7aea0132d34324fe4d304f6f31496d4fb9cc6a7
igor test <igor.patseyko@mail.ru>
2023-09-02 23:12:03 +0200

    test jococo5

--- src/test/java/tests/StatisticsServiceTest.java
+++ src/test/java/tests/StatisticsServiceTest.java
@@ -1,25 +1,43 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxWithSingleValue() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomes = {5};
+        long expected = 5;
 
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithDescendingValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        long expected = 10;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithEqualValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {7, 7, 7, 7, 7};
+        long expected = 7;
+
+        long actual = service.findMax(incomes);
 
         Assertions.assertEquals(expected, actual);
     }
 }
-
-
-
-
-
