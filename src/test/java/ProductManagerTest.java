6940a58b749212a7fc473ba3fcf9de451309a684
igor test <igor.patseyko@mail.ru>
2023-09-04 21:14:31 +0200

    test 12

---
+++ src/test/java/ProductManagerTest.java
@@ -0,0 +1,97 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.Domain.Product;
+import ru.netology.repository.ProductRepository;
+import ru.netology.manager.ProductManager;
+
+public class ProductManagerTest {
+    ProductRepository repo = new ProductRepository();
+    ProductManager manager = new ProductManager(repo);
+
+    Product product1 = new Product(3, "Book 1", 120);
+    Product product2 = new Product(8, "Book 2", 110);
+    Product product3 = new Product(11, "Book 3", 115);
+    Product product4 = new Product(12, "Book 4", 120);
+    Product product5 = new Product(16, "Book 5", 95);
+
+    @BeforeEach
+    public void setup() {
+        manager.add(product1);
+        manager.add(product2);
+        manager.add(product3);
+    }
+
+    @Test
+    public void shouldFindAll() {
+
+        Product[] expected = {product1, product2, product3};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchOneProduct() {
+        manager.add(product4);
+        manager.add(product5);
+
+        Product[] expected = {product4};
+        Product[] actual = manager.searchBy("Book 4");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchMultipleProduct() {
+        manager.add(product4);
+        manager.add(product5);
+
+        Product[] expected = {product1, product2, product3, product4, product5};
+        Product[] actual = manager.searchBy("Book");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchWhenNull() {
+        manager.add(product4);
+        manager.add(product5);
+
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Smartphone");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    public static class ProductRepositoryTest {
+        ProductRepository repo = new ProductRepository();
+
+        Product product1 = new Product(3, "Book 1", 120);
+        Product product2 = new Product(8, "Book 2", 110);
+        Product product3 = new Product(11, "Book 3", 115);
+        Product product4 = new Product(12, "Book 4", 120);
+        Product product5 = new Product(16, "Book 5", 95);
+
+        @BeforeEach
+        public void setup() {
+            repo.save(product1);
+            repo.save(product2);
+            repo.save(product3);
+            repo.save(product4);
+            repo.save(product5);
+        }
+
+        @Test
+        public void shouldRemoveBiId() {
+            repo.removeById(12);
+
+            Product[] expected = {product1, product2, product3, product5};
+            Product[] actual = repo.findAll();
+
+            Assertions.assertArrayEquals(expected, actual);
+        }
+    }
+}
